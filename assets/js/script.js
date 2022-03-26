80383bd7744757c5e3d5f98cef3bfb47b96cafa9
Jose Zuniga <josezuniga@joses-macbook.myfiosgateway.com>
2022-03-26 18:07:48 -0400

    added basic HTML lines for website + js questions for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var jsQuestions = [
+    //questions to store 
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "DOM is an abreviation for ____",
+      choices: ["Data Object Mode", "Dumb Old Man", "Document Object Model", "Dutle Opo Mipsy"],
+      answer: "Document Object Model"
+    },
+    {
+      title: "JavaScript is Textile Mark Up (TML) version of Java?",
+      choices: ["True", "False"],
+      answer: "False"
+    },
+    {
+      title: "JavaScript is strongly typed language",
+      choices: ["True", "False"],
+      answer: "False"
+    }
+  ];
